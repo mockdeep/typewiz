f2dde08b193cd595e23e25091577e2c7a9f67547
Uri Shaked <uri@urishaked.com>
2018-01-17 03:14:45 +0200

    refactor: split integration test
    split the integration test into smaller, more specific test cases

--- src/integration.spec.ts
+++ src/integration.spec.ts
@@ -12,78 +12,109 @@ jest.doMock('fs', () => mockFs);
 
 import { applyTypes, getTypeCollectorSnippet, instrument } from './index';
 
+function typeWiz(input: string) {
+    // Step 1: instrument the source
+    const instrumented = instrument(input, 'c:\\test.ts');
+
+    // Step 2: compile + add the type collector
+    const compiled = transpileSource(instrumented, 'test.ts');
+
+    // Step 3: evaluate the code, and collect the runtime type information
+    const collectedTypes = vm.runInNewContext(getTypeCollectorSnippet() + compiled + '$_$twiz.get();');
+
+    // Step 4: put the collected typed into the code
+    mockFs.readFileSync.mockReturnValue(input);
+    mockFs.writeFileSync.mockImplementationOnce(() => 0);
+
+    applyTypes(collectedTypes);
+
+    if (mockFs.writeFileSync.mock.calls.length) {
+        expect(mockFs.writeFileSync).toHaveBeenCalledTimes(1);
+        expect(mockFs.writeFileSync).toHaveBeenCalledWith('c:\\test.ts', expect.any(String));
+        return mockFs.writeFileSync.mock.calls[0][1];
+    } else {
+        return null;
+    }
+}
+
 describe('integration test', () => {
-    it('should correctly infer and set the types', () => {
+    beforeEach(() => {
+        mockFs.readFileSync.mockImplementation(fs.readFileSync);
+        jest.clearAllMocks();
+    });
+
+    it('should infer `string` type for a simple function', () => {
+        const input = `
+            function greet(c) {
+                return 'Hello ' + c;
+            }
+            greet('World');
+        `;
+
+        expect(typeWiz(input)).toBe(`
+            function greet(c: string) {
+                return 'Hello ' + c;
+            }
+            greet('World');
+        `);
+    });
+
+    it('should correctly handle optional parameters', () => {
         const input = `
             function calculate(a, b?) {
                 return a + (b || 0);
             }
-
-            function greet(c) {
-                return 'Hello ' + c;
-            }
-
-            function commas(item) {
-                return Array.from(item).join(', ');
-            }
-
-            function unused(foo, bar) {
-            }
-
-            function optional(b?, c?) {
-                return b || 0;
-            }
-
             calculate(5, 6);
-            greet('World');
-            commas('Lavender');
-            commas(['Apples', 'Oranges']);
-            optional() + optional(10);
         `;
 
-        const expected = `
+        expect(typeWiz(input)).toBe(`
             function calculate(a: number, b?: number) {
                 return a + (b || 0);
             }
+            calculate(5, 6);
+        `);
+    });
 
-            function greet(c: string) {
-                return 'Hello ' + c;
+    it('should keep unused functions as is', () => {
+        const input = `
+            function unused(foo, bar) {
             }
+        `;
 
+        expect(typeWiz(input)).toBe(null); // null means we haven't updated the input file
+    });
+
+    it('should support union types', () => {
+        const input = `
+            function commas(item) {
+                return Array.from(item).join(', ');
+            }
+            commas('Lavender');
+            commas(['Apples', 'Oranges']);
+        `;
+
+        expect(typeWiz(input)).toBe(`
             function commas(item: string|string[]) {
                 return Array.from(item).join(', ');
             }
-
-            function unused(foo, bar) {
-            }
-
-            function optional(b?: number, c?) {
-                return b || 0;
-            }
-
-            calculate(5, 6);
-            greet('World');
             commas('Lavender');
             commas(['Apples', 'Oranges']);
+        `);
+    });
+
+    it('should should not append `undefined` type to optional parameters', () => {
+        const input = `
+            function optional(b?, c?) {
+                return b || 0;
+            }
             optional() + optional(10);
         `;
 
-        // Step 1: instrument the source
-        const instrumented = instrument(input, 'c:\\test.ts');
-
-        // Step 2: compile + add the type collector
-        const compiled = transpileSource(instrumented, 'test.ts');
-
-        // Step 3: evaluate the code, and collect the runtime type information
-        const collectedTypes = vm.runInNewContext(getTypeCollectorSnippet() + compiled + '$_$twiz.get();');
-
-        // Step 4: put the collected typed into the code
-        mockFs.readFileSync.mockReturnValue(input);
-        mockFs.writeFileSync.mockImplementationOnce(() => 0);
-
-        const result = applyTypes(collectedTypes);
-
-        // Verify that we got the expected result
-        expect(mockFs.writeFileSync).toHaveBeenCalledWith('c:\\test.ts', expected);
+        expect(typeWiz(input)).toBe(`
+            function optional(b?: number, c?) {
+                return b || 0;
+            }
+            optional() + optional(10);
+        `);
     });
 });
