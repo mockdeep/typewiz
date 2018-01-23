66749e498f48e82ec7268178d91a74e440f89a29
Uri Shaked <uri@urishaked.com>
2018-01-23 21:50:43 +0200

    fix(webpack): loader does not instrument the code
    due to a missing return statement

--- packages/typewiz-webpack/src/loader.ts
+++ packages/typewiz-webpack/src/loader.ts
@@ -4,7 +4,7 @@ import { loader } from 'webpack';
 export function typewizLoader(this: loader.LoaderContext, source: string | null) {
     const filename = this.resourcePath;
     if (source && (filename.endsWith('.ts') || filename.endsWith('.tsx'))) {
-        instrument(source, filename);
+        return instrument(source, filename);
     }
     return source;
 }
