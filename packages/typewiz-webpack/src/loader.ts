f333b8736f146f2ee646fc2e59c97f368fc31618
Tom Z?hner <tom.zoehner@gmail.com>
2018-04-02 19:26:03 +0200

    Use typewiz config in typewiz-webpack loader

--- packages/typewiz-webpack/src/loader.ts
+++ packages/typewiz-webpack/src/loader.ts
@@ -1,10 +1,21 @@
-import { instrument } from 'typewiz';
+import { getOptions } from 'loader-utils';
+import * as path from 'path';
+import { ConfigurationParser, instrument } from 'typewiz-core';
 import { loader } from 'webpack';
 
-export function typewizLoader(this: loader.LoaderContext, source: string | null) {
+export async function typewizLoader(this: loader.LoaderContext, source: string | undefined) {
+    const callback = this.async();
+
+    const typewizConfigPath = path.resolve(getOptions(this).typewizConfig || 'typewiz.json');
+    this.addDependency(typewizConfigPath);
+
+    const configurationParser = new ConfigurationParser();
+    await configurationParser.parse(typewizConfigPath);
+
     const filename = this.resourcePath;
     if (source && (filename.endsWith('.ts') || filename.endsWith('.tsx'))) {
-        return instrument(source, filename);
+        callback!(null, instrument(source, filename, configurationParser.getInstrumentOptions()));
+    } else {
+        callback!(null, source);
     }
-    return source;
 }
