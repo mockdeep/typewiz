26b0ac76ced5a49ff40387b7ec860c3056b79cf6
Tom Z?hner <tom.zoehner@gmail.com>
2018-04-03 09:43:02 +0200

    Only parse configuration once in loader

--- packages/typewiz-webpack/src/loader.ts
+++ packages/typewiz-webpack/src/loader.ts
@@ -3,14 +3,23 @@ import * as path from 'path';
 import { ConfigurationParser, instrument } from 'typewiz-core';
 import { loader } from 'webpack';
 
+let promise: Promise<void>;
+let configurationParser: ConfigurationParser;
+
 export async function typewizLoader(this: loader.LoaderContext, source: string | undefined) {
     const callback = this.async();
 
     const typewizConfigPath = path.resolve(getOptions(this).typewizConfig || 'typewiz.json');
     this.addDependency(typewizConfigPath);
 
-    const configurationParser = new ConfigurationParser();
-    await configurationParser.parse(typewizConfigPath);
+    if (promise) {
+        await promise;
+    }
+    if (!configurationParser) {
+        configurationParser = new ConfigurationParser();
+        promise = configurationParser.parse(typewizConfigPath);
+        await promise;
+    }
 
     const filename = this.resourcePath;
     if (source && (filename.endsWith('.ts') || filename.endsWith('.tsx'))) {
