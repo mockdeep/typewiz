c40c0f6140e43fe8de77891d7449c7e8964b8d35
Uri Shaked <uri@urishaked.com>
2018-04-27 00:59:44 +0300

    fix #68: typewiz-node cli broken

--- packages/typewiz-node/src/typewiz-node.ts
+++ packages/typewiz-node/src/typewiz-node.ts
@@ -5,4 +5,4 @@ import { register } from './index';
 register();
 
 // Delegate the rest of the fun to ts-node:
-require('ts-node/dist/_bin'); // tslint:disable-line:no-var-requires
+require('ts-node/dist/bin'); // tslint:disable-line:no-var-requires
